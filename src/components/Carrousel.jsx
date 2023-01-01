bc81741757026af831cc6ec204081789968172a8
Molly Felts Vallin <mnfelts@gmail.com>
2022-12-31 18:06:09 -0800

    logement carrousel images
    - display images from .json array
    - working previous/next arrow controls
    - <p> showing current image index
    
    P7 for OpenClassrooms web developer program

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -9,8 +9,10 @@ function Carrousel({ logementId }) {
   const [current, setCurrent] = useState(0);
   const logement = logementList.filter((logement => logement.id === logementId));
 
-  const slides = logement.map(logement => logement.pictures[0])
+  const slides = logement[0].pictures
   const slidesTotal = slides.length
+  console.log(slides)
+  console.log(slidesTotal)
 
   const nextSlide = () => {
     setCurrent(current === slidesTotal - 1 ? 0 : current + 1)
@@ -25,7 +27,7 @@ function Carrousel({ logementId }) {
       <div className="carrousel_container">
         <div className="carrousel_images">
           {slides.map((picture, index) => (
-            <div className={index === current ? 'image_active' : 'image_hidden'}>
+            <div className={index === current ? 'image_active' : 'image_hidden'} key={index}>
               <img src={picture} alt={"photo de logement " + index} />
             </div>
           ))}
@@ -38,7 +40,7 @@ function Carrousel({ logementId }) {
             <img src={next} alt="flèche suivant" />
           </div>
         </div>
-        <p className="slide_index">{`${current} / ${slidesTotal}`}</p>
+        <p className="slide_index">{`${current + 1} / ${slidesTotal}`}</p>
       </div>
     </section >
   )
