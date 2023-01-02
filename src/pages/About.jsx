2b05bf33ca52b1d972f5f4f1e746769e59a720d5
Molly Felts Vallin <mnfelts@gmail.com>
2023-01-02 10:21:44 -0800

    About details
    - added accordion components to 'about/acceuil' page
    - content from figma mock-up
    
    banner.css updated to work within 'main_container'
    
    P7 for OpenClassrooms web developer program

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,20 @@
 import Banner from "../components/Banner";
 import "../styles/banner.css";
 import bannerImage from "../assets/banner-about.png"
+import Accordion from "../components/Accordion";
 
 function About() {
+
   return (
-    <Banner text={''} imageSrc={bannerImage} imageAlt='mountains' />
+    <div className="container_main">
+      <Banner text={''} imageSrc={bannerImage} imageAlt='mountains' />
+      <div className="container_about">
+        <Accordion key="1" title="Fiabilité" content="" />
+        <Accordion key="2" title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+        <Accordion key="3" title="Service" content="" />
+        <Accordion key="4" title="Responsibilité" content="" />
+      </div>
+    </div>
   );
 }
 
