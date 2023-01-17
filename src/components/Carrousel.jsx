0d68400159903052c44eef87365db6b546ac9e7f
Molly Felts Vallin <mnfelts@gmail.com>
2023-01-17 18:43:10 +0100

    carrousel controls/arrows
    - Logement page: carrousel does not render controls if array length is 1 or 0
    - if length is 0, renders placeholder color with text indicating no images exist
    
    P7 for OpenClassrooms web developer program

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -20,26 +20,53 @@ function Carrousel({ logementId }) {
     setCurrent(current === 0 ? slidesTotal - 1 : current - 1)
   };
 
-  return (
-    <section className="carrousel">
-      <div className="carrousel_images">
-        {slides.map((picture, index) => (
-          <div className={index === current ? 'image_active' : 'image_hidden'} key={index}>
-            <img src={picture} alt={"photo de logement " + index} />
+  if (slidesTotal === 1) {
+    return (
+      <section className="carrousel">
+        <div className="carrousel_images">
+          <div className="image_active">
+            <img src={logement[0].pictures} alt={logement[0].title} />
           </div>
-        ))}
-      </div>
-      <div className="carrousel_controls">
-        <div className="controls_prev" onClick={previousSlide}>
-          <img src={arrow} alt="flèche précédente" />
         </div>
-        <div className="controls_next" onClick={nextSlide}>
-          <img src={arrow} alt="flèche suivant" />
+      </section>
+    )
+  }
+
+  if (slidesTotal === 0) {
+    return (
+      <section className="carrousel">
+        <div className="carrousel_images">
+          <div className="image_placeholder">
+            <p><strong>Sacre Bleu!</strong></p>
+            <p>nous n'avons pas d'images de cette propriété</p>
+          </div>
         </div>
-      </div>
-      <p className="slide_index">{`${current + 1} / ${slidesTotal}`}</p>
-    </section>
-  )
+      </section >
+    )
+  }
+
+  else {
+    return (
+      <section className="carrousel">
+        <div className="carrousel_images">
+          {slides.map((picture, index) => (
+            <div className={index === current ? 'image_active' : 'image_hidden'} key={index}>
+              <img src={picture} alt={`${logement[0].title} ${current + 1}`} />
+            </div>
+          ))}
+        </div>
+        <div className="carrousel_controls">
+          <div className="controls_prev" onClick={previousSlide}>
+            <img src={arrow} alt="flèche précédente" />
+          </div>
+          <div className="controls_next" onClick={nextSlide}>
+            <img src={arrow} alt="flèche suivant" />
+          </div>
+        </div>
+        <p className="slide_index">{`${current + 1} / ${slidesTotal}`}</p>
+      </section>
+    )
+  }
 };
 
 export default Carrousel;
